13acc064665e047d36ab06f949be1df99c071b3d
KoifishVEVO <149301244+KoifishVEVO@users.noreply.github.com>
2025-04-16 18:42:40 +0700

    memperbaiki comments, code sekarang sudah self documenting, maka tidak diperlukan comment terlalu deskriptif

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -63,10 +63,6 @@ private static final double foreigner_salary = 1.5;
         children = new LinkedList<>();
     }
 	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
 	
 	public void setMonthlySalary(EmployeeGrade grade) {
     int newSalary = grade.getBaseSalary();
@@ -104,7 +100,6 @@ private static final double foreigner_salary = 1.5;
 	
 	
 	public int getAnnualIncomeTax() {
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
     monthWorkingInYear = calculateMonthWork();
     boolean hasNoSpouse = (spouse == null);
     return TaxFunction.calculateTax(
